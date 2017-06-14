eb1961690266640ac9fffa3e018934fd6a2dcc71
Eric Rozell <erozell@outlook.com>
2017-06-14 15:30:19 -0400

    stuff.

--- test.py
+++ test.py
@@ -1,10 +1,19 @@
-from pyspark import SparkConf, SparkContext
+from __future__ import print_function
 
-def main(sc):
-  textFile = sc.textFile('wasb://hdfs@u07rlumajqacgvoagnt1.blob.core.windows.net/pg100.txt.utf8')
-  print textFile.count()
+import sys
+from random import random
+from operator import add
+
+from pyspark.sql import SparkSession
+
 
 if __name__ == "__main__":
-  conf = SparkConf()
-  sc = SparkContext(conf)
-  main(sc)
+    spark = SparkSession\
+        .builder\
+        .appName("PythonTest")\
+        .getOrCreate()
+
+    count = spark.sparkContext.textFile('wasb://hdfs@u07rlumajqacgvoagnt1.blob.core.windows.net/pg100.txt.utf8').count()
+    print("Output is %f" % count)
+
+    spark.stop()
